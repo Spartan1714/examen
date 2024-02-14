434603ef931fc05a451fd9965d3091823f5e24b5
Spartan1714 <javierriveraaguilera504@gmail.com>
2024-02-14 01:45:02 -0600

    cambios

--- app/src/main/java/com/example/examen/MainActivity.java
+++ app/src/main/java/com/example/examen/MainActivity.java
@@ -6,12 +6,37 @@ import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PICK_IMAGE_REQUEST = 1;
+    public void selectImageFromGallery(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            // Aquí puedes usar la Uri para mostrar la imagen seleccionada en tu aplicación
+            // Por ejemplo, puedes cargar la imagen en un ImageView
+            ImageView imageView = findViewById(R.id.imageView);
+            imageView.setImageURI(uri);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
